1e3debf3872eca302dd7a2b8e134507bea64a88f
Eruz Vinicius Huffner Lourenço <eruzvinicius@hotmail.com>
2022-03-06 10:15:49 -0300

    Implementação do Exercício 01

--- src/br/com/desafio/model/Exercicio01.java
+++ src/br/com/desafio/model/Exercicio01.java
@@ -1,12 +1,69 @@
 package br.com.desafio.model;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author eruzv
  */
 public class Exercicio01 {
     
-    public static void main(String[] args) {
+    /**
+     * 
+     * @param arrayNumeros array de inteiros desordenados
+     * @return mediana     elemento central do array ordenado  
+     */
+    public static int retornaMediana(int [] arrayNumeros){
         
+        int tamanhoArray;
+        int posicaoMediana;
+        int mediana;
+        
+        Arrays.sort(arrayNumeros);
+        tamanhoArray = arrayNumeros.length;
+
+        posicaoMediana = (int) Math.ceil(tamanhoArray/2);        
+        mediana = arrayNumeros[posicaoMediana];
+        
+        return mediana;
+    }
+     
+    public static void main(String[] args) {
+        int tamanho;             
+        Scanner sc = new Scanner(System.in);
+        
+        int[] array = {9,2,1,4,6};
+        
+        
+//        //solicita tamanho para o array;;
+//        do{
+//          System.out.print("Insira um tamanho ímpar para o array: ");
+//          tamanho = sc.nextInt();
+//        }while(tamanho % 2 == 0 );
+// 
+//        //cria o array com o tamanho especificado
+//        int[] array = new int[tamanho];
+//              
+//        //solicita input para o array
+//        System.out.println("Preencha o array com números inteiros: ");
+//        for (int i =0; i < tamanho; i++){
+//           System.out.print("array[" + i + "] = "); 
+//           array[i] = sc.nextInt();         
+//        }
+//
+//        //imprime o array na tela
+//        System.out.print("Array = {");
+//        for (int i =0; i < tamanho; i++){
+//            if(i == tamanho - 1){
+//                System.out.print(array[i]);   
+//            }else{
+//                System.out.print(array[i]+",");       
+//            }   
+//        }
+//        System.out.println("}");
+       
+        //imprime a mediana
+        System.out.println(retornaMediana(array));   
     }    
 }
